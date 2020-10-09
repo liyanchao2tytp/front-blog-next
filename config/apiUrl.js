db3e9844ee000a0e28e9ae09003eab7afc7c1e62
liyanchao2tytp <57429651+liyanchao2tytp@users.noreply.github.com>
2020-10-09 08:39:53 +0800

    在项目中分离后台egg.js，并把写死的数据改成了从数据库中请求的数据

---
+++ config/apiUrl.js
@@ -0,0 +1,10 @@
+let ipUrl = 'http://127.0.0.1:7001/'
+
+let servicePath = {
+  getArticleList: ipUrl + 'getlist',  // 首页接口
+  getArticleById: ipUrl + 'getById',  // 详细页接口
+  getTypeInfo: ipUrl + 'getType',     // 获取导航栏在数据库中的所有数据
+  getList: ipUrl + 'getAlist'
+}
+
+export default servicePath
